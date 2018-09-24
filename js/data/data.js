babf89fc2d1776076621f2008acf38d19abba808
HelgaN <ola14@tut.by>
2018-09-24 14:24:17 +0300

    task 5.2

--- js/data/data.js
+++ js/data/data.js
@@ -1,9 +1,12 @@
 const initialState = Object.freeze({
-  level: `level-artist`,
+  level: `level-0`,
   mins: `05`,
   secs: `00`,
-  lives: 3
+  lives: 3,
+  questions: [`level-0`, `level-1`],
+  stats: Array(2).fill(`unknown`)
 });
+
 /*
 const level = Object.freeze({
   title: `Кто исполняет эту песню?`,
@@ -11,12 +14,23 @@ const level = Object.freeze({
 });*/
 
 const levels = Object.freeze({
-  'level-artist': {
+  'level-0': {
+    type: `level-artist`,
     title: `Кто исполняет эту песню?`,
+    question: {
+      src: `sound/track1.mp3`,
+      answer: `Пелагея`
+    },
     answers: new Set([`Пелагея`, `Краснознаменная дивизия имени моей бабушки`, `Lorde`])
   },
-  'level-genre': {
+
+  'level-1': {
+    type: `level-genre`,
     title: `Выберите инди-рок треки`,
+    question: {
+      src: [`sound/track1.mp3`, `sound/track2.mp3`, `sound/track3.mp3`, `sound/track4.mp3`],
+      answers: [false, true, true, false]
+    },
     answers: new Set([`a-1`, `a-2`, `a-3`, `a-4`])
   }
 });
